5963699e2fecb52f1b8f61079fa4b4538beb392b
Tim Mayberry <mojofunk@gmail.com>
2015-05-01 12:41:07 +1000

    Remove unnecessary output

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ############################
-# .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles
 # in ~/Data/dotfiles
 ############################
@@ -16,12 +15,10 @@ files="vimrc vim gitconfig"       # list of files/folders to symlink
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create
 # symlinks 
